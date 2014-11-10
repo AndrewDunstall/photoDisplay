2890d318f99f7a561b8e6b6056066d78f89b3853
Andrew Dunstall <andrew.dunstall@gmail.com>
2014-11-10 23:46:52 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,125 @@
+<?PHP
+//session_destroy();
+$page_title = 'photoDisplay';
+include ('header.html');
+//------------------------------------------------------------------
+?>
+<div id="main2">
+
+<div id="rightcolumn">
+ 
+
+
+
+<div class="inner">
+<h3>Big</h3>
+<p>Offering both online and hardcopy
+proofing from your images.</p> 
+<p class="more"><a href="iproofing.php" alt="vaultdigital online proofing" >read more</a></p>
+</div>
+
+
+<div class="inner">
+<h3>Bigger</h3>
+<p>Once images are chosen.</p>
+<p class="more"><a href="iprocessing.php"alt="vaultdigital processing">read more</a></p>
+</div>
+          
+
+<div class="inner">
+<h3>Biggest</h3>
+<p>Safe and secure.</p>
+<p class="more"><a href="index.php" alt="vaultdigital image file archiving">read more</a>
+
+</div>
+
+</div> <!-- rightcolumn -->
+
+
+
+
+
+
+
+
+
+<div id="leftcolumn">
+
+<div class="inner">
+<h3>WHAT Else?</h3>
+<p>Nothing much.</p>
+
+</div>
+
+
+
+<div class="box">
+<h3>LOGIN?</h3>
+<p>To gain access to your
+PhotoBOOTH <p class="more"><a href="photoboothlogon.php" alt="photoDisplay" >click here</a></p>
+<p>Or to visit a Photographers
+Selection <p class="more"><a href="selection.php" alt="photoDisplay" >click here</a></p>
+</div>
+          
+</div> <!-- leftcolumn  -->
+
+
+
+
+
+
+
+
+
+
+
+
+<div id="content">
+
+
+
+
+<div id="mainfeature">
+<h2>photoDisplay</h2>
+<p>photoDisplay can take a folder of photos and display them on a page 
+so you can choose them. Each thumbnail has a checkbox next to it - for ordering perhaps.</p>
+<p>If you click on a thumbnail, the large version of the photo opens.</p>
+
+
+<h1> I need to finish this code - it needs a mysql DB to run </h1>
+
+
+
+</div> <!-- mainfeature -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div> <!-- ----------------------------------------    content -->
+
+</div> <!-- main2 -->
+
+<?PHP
+//-------------------------------------------------------------------------------------------------
+include ('footer.html');
+?>
