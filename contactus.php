f05a6bec5b719d0767621e357ed135e3440367d2
Andrew Dunstall <andrew.dunstall@gmail.com>
2014-11-10 23:44:09 +1100

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,42 @@
+<?PHP
+$page_title = 'Contact Us';
+include ('header.html');
+//------------------------------------------------------------------
+?>
+
+<div id="main2">
+
+<div class="text">
+
+<br />
+<br />
+<br />
+<br />
+
+<h1>Contact Us:</h1>
+
+
+<br />
+
+<h3>Operating Hours:</h3>
+Monday to Friday 9.00am to 5.00pm<br />
+
+
+<br />
+
+
+
+
+</div> 
+
+<br />
+<br />
+
+</div> <!-- main -->
+
+
+<?PHP
+//-------------------------------------------------------------------------------------------------
+include ('footer.html');
+?>
+
