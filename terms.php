e74b5681724196694d09c648f93c42ebe9135cf5
Andrew Dunstall <andrew.dunstall@gmail.com>
2014-11-10 23:51:35 +1100

    Create terms.php

---
+++ terms.php
@@ -0,0 +1,33 @@
+<?PHP
+$page_title = 'Terms & Conditions';
+include ('header.html');
+//------------------------------------------------------------------
+?>
+
+<div class="text">
+
+<br />
+<br />
+<br />
+
+<h1>Terms and Conditions:</h1>
+<br />
+<h3>Processing:</h3>
+
+
+<form>
+<input type="button" class="buttonleft" value="BACK" onClick="javascript:history.go(-1)" />
+</form>
+
+<br />
+<br />
+
+
+</div>
+
+
+<?PHP
+//-------------------------------------------------------------------------------------------------
+include ('footer.html');
+?>
+
