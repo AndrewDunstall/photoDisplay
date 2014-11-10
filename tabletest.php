f586fd9f5ec53c738352d704bb0a8a7d8fae5470
Andrew Dunstall <andrew.dunstall@gmail.com>
2014-11-10 23:51:13 +1100

    Create tabletest.php

---
+++ tabletest.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+	<head>
+		<title>Untitled Page</title>
+	</head>
+
+	<body bgcolor="#ffffff">
+		<p></p>
+	</body>
+
+</html>
